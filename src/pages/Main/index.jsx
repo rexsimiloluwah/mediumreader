c13be9e2d9403dcc8e83f99dadf8ea72156f023c
rexsimiloluwah <adetoyosi2001@gmail.com>
2021-01-14 02:43:26 +0000

    Increased wordcloud size

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -11,9 +11,9 @@ const TextCloud = ({data}) => {
     return(
     <WordCloud 
         width={"auto"} 
-        maxFont={40} 
-        minFont={10} 
-        logFunc={(x)=> Math.log2(x) * 5} 
+        maxFont={50} 
+        minFont={25} 
+        logFunc={(x)=> Math.log2(x) * 10} 
         data={data}
         clickEvent={(x)=>console.log(x.word)} 
         color={['#71803F', '#F8AC1D','#598EC0','#E2543E','#1A3051','#F46F73','#8A87BB','#56CFCD','#297373','#FF8552','#F2E863','#C2F8CB','#3A6EA5','#FF6700','#C0C0C0','#4E4381','#523CBD',]}
